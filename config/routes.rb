262b7f28142ee928552a3182c1f40aa61f705c44
Atsu77 <kamnknon@gmail.com>
2022-01-25 12:42:11 +0900

    add student_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'Student', at: 'auth'
+  namespace :api do
+    mount_devise_token_auth_for 'Student', at: 'auth/students', controllers: {
+      registrations: 'api/auth/students'
+    }
+  end
 end
